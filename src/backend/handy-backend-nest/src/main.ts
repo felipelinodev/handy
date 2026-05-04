d0093288decac1cd5466727a5e8c2a50dd72eca2
felipelinodev <felipelinodesigndoe@gmail.com>
2026-05-03 23:55:21 -0300

    feat: config global prefix api/v1

--- src/backend/handy-backend-nest/src/main.ts
+++ src/backend/handy-backend-nest/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 4001, '0.0.0.0');
 }
 bootstrap();
